1093d5d921126066c3621c279ea925609daa0569
Owais Ali <owaisaliarshad963@gmail.com>
2024-05-12 13:32:59 +0500

    fix: deploy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-router-dom",  '@mui/icons-material',],
+      external: ["react-router-dom",  '@mui',],
      
     },
   },
