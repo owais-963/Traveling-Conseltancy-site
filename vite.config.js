20da0b836899c7e8f89fada772d4d20af5f13343
Owais Ali <owaisaliarshad963@gmail.com>
2024-05-12 13:32:29 +0500

    fix: deploy issue

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-router-dom",  '@mui/icons-material/ArrowForward',],
+      external: ["react-router-dom",  '@mui/icons-material',],
      
     },
   },
